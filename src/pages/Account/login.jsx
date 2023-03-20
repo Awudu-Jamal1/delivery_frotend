4f34d732776ed343a58585f034db6b74785543b4
Awudu-Jamal1 <78368357+Awudu-Jamal1@users.noreply.github.com>
2023-03-20 17:48:11 +0000

    signin

--- src/pages/Account/login.jsx
+++ src/pages/Account/login.jsx
@@ -1,19 +1,21 @@
 export default function Login (){
+    const style = "mt-2 block w-full h-14 order-0 px-3 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6  "
+    const labels ="block text-md font-bold"
     return(
         <>
-        <div className="signup">
-            <div>
-                <h1>Login to your Quickly Account </h1>
+        <div className="signup  w-max py-5">
+            <div className="text-center">
+                <h1 className="font-bold px-20">Login to your Quickly Account </h1>
             </div>
             <div>
-                <form action="" method="post">
-                    <div>
-                        <label htmlFor="">Email Address</label>
-                        <input type="email" placeholder="Enter Email Address" />
+                <form action="" method="post" className="py-5 px-10">
+                    <div className="py-4">
+                        <label htmlFor="" className={labels}>Email Address</label>
+                        <input type="email" className={style} placeholder="Enter Email Address" />
                     </div>
-                    <div>
-                        <label htmlFor="">Password</label>
-                        <input type="password" placeholder="Enter Password" />
+                    <div className="py-4">
+                        <label htmlFor="" className={labels}>Password</label>
+                        <input type="password" className={style} placeholder="Enter Password" />
                     </div>
                 </form>
             </div>
