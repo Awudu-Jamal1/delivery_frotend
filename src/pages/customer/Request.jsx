b9cc17b26dc29bfc90a20f09915ceb5d13dfe945
Awudu-Jamal1 <78368357+Awudu-Jamal1@users.noreply.github.com>
2023-03-21 10:52:44 +0000

    Working on request Page

--- src/pages/customer/Request.jsx
+++ src/pages/customer/Request.jsx
@@ -1,6 +1,29 @@
 export default function Request(){
     return(
         <>
-        this request page</>
+        <div className="flex justify-center ">
+            <div className=" w-[40em] py-10 px-12 bg-my-blue text-white">
+                <div className="text-center">
+                    <h1>Start Request</h1>
+                </div>
+                <div className="w-full ">
+                    {/* <label htmlFor=""></label> */}
+
+
+                    <div className="my-5">
+                    <input type="text" placeholder="Where is Parcel?" name="" id="" className=" px-2 w-full placeholder-[#000] h-12 font-bold " />
+                    </div>
+                    <div className="my-5">
+                    <input type="text" placeholder="Parcel Destination?" name="" id="" className="h-12 px-2 w-full placeholder-[#000] font-bold" />
+                    </div>
+                    <div className="my-5">
+                    <input type="text" placeholder="Weight(kg)" name="" id="" className="h-12 px-2 w-full placeholder-[#000] font-bold" />
+                    </div>
+                    <div className="text-center pt-5 "><button className="bg-green-400 py-2 px-4">Process Request</button></div>
+                </div>
+            </div>
+
+        </div>
+        </>
     )
 }
