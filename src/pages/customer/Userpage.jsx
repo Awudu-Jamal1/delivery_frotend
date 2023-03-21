0b6e013f22eee7e9b845465f3a4d03f4247559b1
Awudu-Jamal1 <78368357+Awudu-Jamal1@users.noreply.github.com>
2023-03-21 09:34:17 +0000

    finished with tracking page

--- src/pages/customer/Userpage.jsx
+++ src/pages/customer/Userpage.jsx
@@ -8,21 +8,24 @@ export default function Userpage() {
 
       <div>
         <div >
-        <div className="border">
+        <div className="">
             <div className="flex justify-center ">
             <Intro/>
             </div>
 
           <div  className="flex justify-center ">
-            <div className="border intro">
-            <div className="grid grid-cols-3 gap-2 border  ">
-                <div className="col-span-2 "><input className=" w-full h-12" type="text" /></div>
-                <div className=" border "><button className="h-12 w-16 bg">TRACK</button></div>
+            <div className="intro pl-20 max-sm:pl-0">
+            <div className="grid grid-cols-3 max-sm:grid-cols-1 gap-2   ">
+                <div className="col-span-2 "><input className=" w-full h-12 bg-[#f7fff7] text-[#000] font-bold px-4 placeholder-[#000] placeholder-opacity-100" placeholder="TRACK ID" type="text" /></div>
+                <div className="max-sm:pl-36  "><button className="h-12 text-white font-bold w-36 text-center bg-red-500 hover:bg-[#023e8a]">TRACK</button></div>
             </div>
-            <div>History</div>
+            <div className="font-bold py-4 text-right pr-20 max-sm:pr-0">DELIVERY HISTORY</div>
             </div>
           </div>
     </div>
+        </div>
+        <div className="flex justify-center my-12">
+          <div><h1 className="font-bold">No Delivery Available </h1></div>
         </div>
       </div>
     </>
