3c79d3ea54d2229c5d7cee829c700dd56badf24f
Awudu-Jamal1 <78368357+Awudu-Jamal1@users.noreply.github.com>
2023-03-21 05:16:05 +0000

    added User page

--- src/pages/customer/Userpage.jsx
+++ src/pages/customer/Userpage.jsx
@@ -1,5 +1,30 @@
-export default function Userpage(){
-   return( <>
-    User
-    </>)
+import accounting from "../../assets/accounting.svg";
+import Intro from "./intro";
+
+export default function Userpage() {
+
+  return (
+    <>
+
+      <div>
+        <div >
+        <div className="border">
+            <div className="flex justify-center ">
+            <Intro/>
+            </div>
+
+          <div  className="flex justify-center ">
+            <div className="border intro">
+            <div className="grid grid-cols-3 gap-2 border  ">
+                <div className="col-span-2 "><input className=" w-full h-12" type="text" /></div>
+                <div className=" border "><button className="h-12 w-16 bg">TRACK</button></div>
+            </div>
+            <div>History</div>
+            </div>
+          </div>
+    </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'my-blue': '#023e8a',
+      }
+    },
   },
   plugins: [],
 }
