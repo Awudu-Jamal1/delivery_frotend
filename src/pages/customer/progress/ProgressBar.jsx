b8f77770b75a598a30f2e752a377599e7ef59b13
Awudu-Jamal1 <78368357+Awudu-Jamal1@users.noreply.github.com>
2023-03-22 08:27:03 +0000

    still on progress bar

--- src/pages/customer/progress/ProgressBar.jsx
+++ src/pages/customer/progress/ProgressBar.jsx
@@ -1,47 +1,24 @@
-import { useState } from "react";
+import Stepper from "./steppers";
 
-import { TiTick } from "react-icons/ti";
-import "./stepper.css";
-
-export default function ProgressBars(){
-
-  const steps = ["Customer Info", "Shipping Info", "Payment", "Step 4"];
-    const [currentStep, setCurrentStep] = useState(1);
-  const [complete, setComplete] = useState(false);
-
-    return(
-        <>
-        <div><h1 className="font-bold">Progress Bar </h1></div>
-       <div>
-       <div className="flex justify-between">
-        {steps?.map((step, i) => (
-          <div
-            key={i}
-            className={`step-item ${currentStep === i + 1 && "active"} ${
-              (i + 1 < currentStep || complete) && "complete"
-            } `}
-          >
-            <div className="step">
-              {i + 1 < currentStep || complete ? <TiTick size={24} /> : i + 1}
-            </div>
-            <p className="text-gray-500">{step}</p>
-          </div>
-        ))}
+export default function ProgressBars() {
+  return (
+    <>
+    <div >
+    <div >
+        <h1 className="font-bold text-center">Progress Bar </h1>
       </div>
-      {!complete && (
-        <button
-          className="btn"
-          onClick={() => {
-            currentStep === steps.length
-              ? setComplete(true)
-              : setCurrentStep((prev) => prev + 1);
-          }}
-        >
-          {currentStep === steps.length ? "Finish" : "Next"}
-        </button>
-      )}
-       </div>
+      <div className="bg-[#f7fff7]  py-10 my-6">
+        <div className="">
+          <div className="pl-12 my-4 ">
+          <p>Order   #15F621</p>
+          <p className="leading-loose font-bold">Placed in June 1 ,2023</p>
+          </div>
 
-        </>
-    )
+        </div>
+        <Stepper/>
+      </div>
+    </div>
+
+    </>
+  );
 }
