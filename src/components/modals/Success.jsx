c2b94e9ceb9dae2923986871c6287d1a721324d0
Awudu-Jamal1 <78368357+Awudu-Jamal1@users.noreply.github.com>
2023-03-23 21:50:38 +0000

    Added Agent Page

--- src/components/modals/Success.jsx
+++ src/components/modals/Success.jsx
@@ -41,7 +41,7 @@ export default function Success() {
                     </div>
                     <div className="mt-3 border-slate-900 text-center sm:mt-0 sm:ml-4 sm:text-left">
                       <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
-                        Success
+                        Success tip
                       </Dialog.Title>
                       <div className="mt-2">
                         <p className="text-sm text-gray-500">
