9fc3835d4772d14db0184ead94e523c8e9e53101
Awudu-Jamal1 <78368357+Awudu-Jamal1@users.noreply.github.com>
2023-03-31 13:54:50 +0100

    vaulidate

--- src/pages/Account/User.jsx
+++ src/pages/Account/User.jsx
@@ -13,7 +13,9 @@ export default function User (){
         compare:''
 
     })
-    const {register,handleSubmit,formState:{errors}}= useForm()
+    const {register,handleSubmit,formState:{errors},watch}= useForm()
+    // console.log(register
+    //     )
     const style = "mt-2 block w-full order-0 px-3 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6  "
     const labels ="block text-md font-bold"
     const estyle ="mt-2 block w-full order-0 px-3 bg-rose-100 py-1.5 text-gray-900 ring-1 ring-inset ring-red-600 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6  "
@@ -70,7 +72,10 @@ export default function User (){
                         </div>
                         <div>
                             <label htmlFor=""className={labels}>Confirm Password</label>
-                            <input autoComplete="false" {...register('compare',{required:true,minLength:8})}  autoSave="false" type="password" className={style} placeholder="Confirm Password" />
+                            <input autoComplete="false" {...register('compare',{required:true,minLength:8,validate:(val)=>{
+                                if(watch('password')!== val)return "Your Password do not Match"
+                            }})}  autoSave="false" type="password" className={errors.compare?estyle:style} placeholder="Confirm Password" />
+                         {errors.compare &&<p className="text-rose-600 font-bold text-[0.8em] px-2 py-1 "> {errors.compareare?.message}</p>}
                         </div>
                     </div>
                 </form>
