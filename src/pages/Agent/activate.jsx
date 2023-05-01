d332797210cd59497ddb818e821d138106e6229f
Awudu-Jamal1 <78368357+Awudu-Jamal1@users.noreply.github.com>
2023-05-01 06:11:11 +0100

    another

--- src/pages/Agent/activate.jsx
+++ src/pages/Agent/activate.jsx
@@ -1,13 +1,23 @@
-import { useLoaderData } from 'react-router-dom';
-import Delivery from './delivery';
+import { useLoaderData } from "react-router-dom";
+import Delivery from "./delivery";
 function Activate() {
-    const data = useLoaderData();
-    const datas = data ?data.orders: []
-    const evals = datas.length === 0 ? true :false
-    return ( <>
-    { evals && <div className='text-center mt-7'><h1>No Request Available</h1></div>}
-    {datas?.map((e,i)=><div  key={i}><Delivery data={e}/></div>)}
-    </> );
+  const data = useLoaderData();
+  const datas = data ? data.orders : [];
+  const evals = datas.length === 0 ? true : false;
+  return (
+    <>
+      {evals && (
+        <div className="text-center mt-7">
+          <h1>No Request Available</h1>
+        </div>
+      )}
+      {datas?.map((e, i) => (
+        <div key={i}>
+          <Delivery data={e} />
+        </div>
+      ))}
+    </>
+  );
 }
 
 export default Activate;
