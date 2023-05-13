4655f3f40178e3af09b28c00e7838181c7c33358
Awudu-Jamal1 <78368357+Awudu-Jamal1@users.noreply.github.com>
2023-05-13 04:41:24 +0100

    added transit file

--- src/pages/customer/Progressin.jsx
+++ src/pages/customer/Progressin.jsx
@@ -8,65 +8,38 @@ function Progressin() {
     const location = useLocation()
     console.log(location.state)
     return ( <>
-    {/* <div className="mt-[3em] flex justify-center "><div className=" " >
-       <div className="flex justify-center w-[30em]"> <div  className="grid grid-cols-3 py-2 px-2">
-        <div className="flex self-center"><HiArrowLeftCircle/></div>
-        <div><h2>Details</h2></div>
-        <div></div>
-        </div></div>
-        <div  className="flex border " >
-            <div>
-            <div className="">
-                1234561789
-            </div>
-            <div>
-                <div><h2>Rider Details</h2></div>
-                <div>Name</div>
-                <div>Number</div>
-                <div>Bic Number</div>
-            </div>
 
 
-            </div>
-
-        </div>
-        <div>
-                <div className="w-[30em]">
-                <ProgressBars/>
-                </div>
-
-            </div>
-
-    </div>
-        </div> */}
-
-       <div>
+       <div className="mt-[5em] text-[#000]">
 <div>
-    <div className="flex justify-center "> <div  className="grid grid-cols-3 py-2 px-2">
-        <div className="flex self-center"><HiArrowLeftCircle/></div>
-        <div><h2>Details</h2></div>
+    <div className="flex justify-center "> <div  className="grid grid-cols-3 py-4 px-5  bg-[#fff] w-[29em]">
+        <div className="flex self-center "><HiArrowLeftCircle className="text-[1.5em] "/></div>
+        <div><h2 className="font-bold">Details</h2></div>
         <div></div>
         </div></div>
 
 </div>
-<div  className="flex justify-center " >
-            <div className="border w-[32em]">
-            <div className="py-2">
+<div  className="flex justify-center  " >
+            <div className="w-[29em] bg-[#fff]">
+            <div className="py-4 px-8 font-semibold">
                 1234561789
             </div>
-            <div className="py-2 text-[0.8em]">
-                <div><h2>Rider Details</h2></div>
-                <div>Name</div>
+            <div className="py-4 px-8">
+                <div><h2 className="text-[0.9em]">Rider Details</h2></div>
+                <div className=" text-[0.8em] py-3">
+                <div >Name</div>
                 <div>Number</div>
                 <div>Bic Number</div>
+                </div>
+
             </div>
 
 
             </div>
 
         </div>
-<div className="flex justify-center ">
-    <div className=" w-[29em]">
+<div className="flex justify-center  ">
+    <div className=" w-[29em] bg-[#fff] py-5">
     <Stepper/>
 
     </div>
