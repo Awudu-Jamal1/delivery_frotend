9e03b77844fbf780b1679efa84873a243cdb5132
BossJ-419 <78368357+BossJ-419@users.noreply.github.com>
2024-03-04 18:37:20 +0000

    next

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 export default()=>{
     return axios.create({
-        baseURL:import.meta.env.VITE_APP_BASE_URL
+        baseURL:process.env.VITE_APP_BASE_URL
     })
 }
--- src/services/socket.js
+++ src/services/socket.js
@@ -1,5 +1,5 @@
 import {io} from "socket.io-client";
 
 export default()=>{
-    return io(import.meta.env.VITE_APP_BASE_URL)}
+    return io(process.env.VITE_APP_BASE_URL)}
 // const socket  =
