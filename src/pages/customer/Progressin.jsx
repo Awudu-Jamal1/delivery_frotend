5e407edb1f23abf6e41bfc1df9dc79bb9a2df143
BossJ-419 <78368357+BossJ-419@users.noreply.github.com>
2024-05-20 16:29:08 +0000

    error search

--- src/pages/customer/Progressin.jsx
+++ src/pages/customer/Progressin.jsx
@@ -6,9 +6,9 @@ import ProgressBars from "./progress/ProgressBars";
 
 function Progressin() {
     const location = useLocation()
-    const info =location.state
-    const riders =info.rider
-    console.log(info,riders)
+    // const info =location.state
+    // const riders =info.rider
+    // console.log(info,riders)
     return ( <>
   
 
@@ -28,13 +28,13 @@ function Progressin() {
                 {info.id}
             </div>
             <div className="py-4 px-8">
-                <div><h2 className="text-[0.9em]">Rider Details</h2></div>
+                {/* <div><h2 className="text-[0.9em]">Rider Details</h2></div>
                 <div className=" text-[0.8em] py-3">
                 <div className=" py-1">{ riders.firstName +" "+ riders.lastName}</div>
                 <div className=" py-1" >0{riders.phone}</div>
                 <div className=" py-1">{}</div>
                 <div>{riders.length ===0 &&<p>Searching for Agent</p>}</div>
-                </div>
+                </div> */}
 
             </div>
 
@@ -44,7 +44,7 @@ function Progressin() {
         </div>
 <div className="flex justify-center  ">
     <div className=" w-[29em] bg-[#fff] py-5">
-    <Stepper id={info.ids} status={info.status}/>
+    {/* <Stepper id={info.ids} status={info.status}/> */}
 
     </div>
  </div>
