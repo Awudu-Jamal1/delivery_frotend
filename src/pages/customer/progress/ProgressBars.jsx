87ec3886f96f1be6bb24e008f0b907c6c7c0a775
BossJ-419 <78368357+BossJ-419@users.noreply.github.com>
2024-05-20 12:50:07 +0000

    console res

--- src/pages/customer/progress/ProgressBars.jsx
+++ src/pages/customer/progress/ProgressBars.jsx
@@ -17,7 +17,7 @@ export default function ProgressBars({ id, type }) {
 const role = 'Customer'
   let cm;
   console.log("emit", emit)
-  console.log("catchs",catchs)
+  console.log("catchs",res)
 
   useEffect(() => {
     let fetched = async () => {
